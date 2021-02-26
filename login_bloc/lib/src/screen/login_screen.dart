270b3c278e8cb149fa8a91003d6b00b4c0bf7e81
Pratik Pathak <46027754+pratikpathak99@users.noreply.github.com>
2021-02-26 19:53:39 +0530

    Update login_screen.dart

--- login_bloc/lib/src/screen/login_screen.dart
+++ login_bloc/lib/src/screen/login_screen.dart
@@ -3,10 +3,40 @@ import 'package:flutter/material.dart';
 class LoginScreen extends StatelessWidget{
   Widget build(context){
     return Container(
-      margin: EdgeInsets.all(20.0),
+      margin: EdgeInsets.all(50.0),// set the margin
       child: Column(
-        children: <Widget>[],
+        children: <Widget>[
+          emailField(),
+          passwordField(),
+          Container (margin: EdgeInsets.only(top: 25.0),),
+          submitButton(),
+        ],
       ),
+    );
+  }
+  Widget emailField(){
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        hintText: 'you@gmail.com',
+        labelText: 'Email Address',
+      ),
+    );
+  }
+  Widget passwordField(){
+    return TextField(
+      obscureText: true,//hide the password
+      decoration: InputDecoration(
+        hintText: 'Enter Your password',
+        labelText: 'Password',
+      ),
+    );
+  }
+  Widget submitButton(){
+    return RaisedButton(
+      child: Text('Login'),
+      color: Colors.blue,
+      onPressed: (){},
     );
   }
 }
